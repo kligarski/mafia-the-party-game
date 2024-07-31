9ceeb274d99c45e6f4ca0ad9a56f54649b42daee
Krzysztof Ligarski <ligarski.k@gmail.com>
2024-07-31 14:41:25 +0200

    layout.html

--- backend/mafia_app/views.py
+++ backend/mafia_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, "index.html")
+def game(request):
+    return render(request, "game.html")
+
+def login(request):
+    return render(request, "login.html")
